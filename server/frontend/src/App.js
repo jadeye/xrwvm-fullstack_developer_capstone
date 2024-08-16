75e89d1dec481f6323782366b7704b94bc7f09de
jadeye <judmojo@gmail.com>
2024-08-16 12:17:00 -0400

    maybe done with Module 4?? 16.8

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import RegisterPanel from "./components/Register/Register";
 import Dealers from "./components/Dealers/Dealers";
 import Dealer from "./components/Dealers/Dealer";
 import { Routes, Route } from "react-router-dom";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
--- server/frontend/src/components/Dealers/PostReview.jsx
+++ server/frontend/src/components/Dealers/PostReview.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import PostReview from "./components/Dealers/PostReview"
 import "./Dealers.css";
 import "../assets/style.css";
 import Header from '../Header/Header';
