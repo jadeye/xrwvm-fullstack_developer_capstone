2fdb0fa0096a8e77303c59b6de7041d3d02b7515
jadeye <judmojo@gmail.com>
2024-08-16 21:58:39 +0300

    Module 5: And (Lint ready) NOW?

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,12 +31,24 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://judmojo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-    'https://judmojo-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://judmojo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01'
+        '.proxy.cognitiveclass.ai'
+    ),
+    (
+        'https://judmojo-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+        '.proxy.cognitiveclass.ai'
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://judmojo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-    'https://judmojo-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    (
+        'https://judmojo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01'
+        '.proxy.cognitiveclass.ai'
+    ),
+    (
+        'https://judmojo-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+        '.proxy.cognitiveclass.ai'
+    ),
 ]
 
 REST_FRAMEWORK = {
@@ -103,17 +115,24 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': (
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
         ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.NumericPasswordValidator'
+        ),
     },
 ]
 
